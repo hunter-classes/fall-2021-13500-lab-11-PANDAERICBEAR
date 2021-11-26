a3011492b8945ce801396dc0995c1b65991c0322
Eric Li <johnlee888123456@gmail.com>
2021-11-26 00:22:47 -0500

    Added Parts, no tests

---
+++ Makefile
@@ -0,0 +1,17 @@
+main: main.o profile.o
+	g++ -o main main.o profile.o
+
+tests: tests.o
+	g++ -o tests tests.o
+
+main.o: main.cpp profile.h
+	g++ -c main.cpp
+
+profile.o: profile.cpp profile.h
+	g++ -c profile.cpp
+
+tests.o: tests.cpp doctest.h
+	g++ -c -std=c++11 tests.cpp
+
+clean:
+	rm -f *.o
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include "profile.h"
+
+int main(){
+  // Profile user("Eric Li", "DUMBPANDABEAR");
+  // std::cout << user.getUsername() << std::endl;
+  //
+  // Profile p1("marco", "Marco");
+  // std::cout << p1.getUsername() << std::endl; // marco
+  // std::cout << p1.getFullName() << std::endl; // Marco (@marco)
+  //
+  // p1.setDisplayName("Marco Rossi");
+  // std::cout << p1.getUsername() << std::endl; // marco
+  // std::cout << p1.getFullName() << std::endl; // Marco Rossi (@marco)
+  //
+  // Profile p2("tarma1", "Tarma Roving");
+  // std::cout << p2.getUsername() << std::endl; // tarma1
+  // std::cout << p2.getFullName() << std::endl; // Tarma Roving (@tarma1)
+
+  Network nw;
+  // add three users
+  nw.addUser("mario", "Mario");
+  nw.addUser("luigi", "Luigi");
+  nw.addUser("yoshi", "Yoshi");
+
+  // make them follow each other
+  nw.follow("mario", "luigi");
+  nw.follow("mario", "yoshi");
+  nw.follow("luigi", "mario");
+  nw.follow("luigi", "yoshi");
+  nw.follow("yoshi", "mario");
+  nw.follow("yoshi", "luigi");
+
+  // add a user who does not follow others
+  nw.addUser("wario", "Wario");
+
+  // add clone users who follow @mario
+  for(int i = 2; i < 6; i++) {
+      std::string usrn = "mario" + std::to_string(i);
+      std::string dspn = "Mario " + std::to_string(i);
+      nw.addUser(usrn, dspn);
+      nw.follow(usrn, "mario");
+  }
+  // additionally, make @mario2 follow @luigi
+  nw.follow("mario2", "luigi");
+
+  nw.printDot();
+  return 0;
+}
