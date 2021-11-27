e7488e98b33d6079b7e69c64d8d33bb398dfa12d
Eric Li <johnlee888123456@gmail.com>
2021-11-26 23:59:33 -0500

    added part C tests

--- tests.cpp
+++ tests.cpp
@@ -37,23 +37,24 @@ TEST_CASE("Task C"){
   nw.addUser("yoshi", "Yoshi");
 
   // make them follow each other
-  nw.follow("mario", "luigi");
-  nw.follow("mario", "yoshi");
-  nw.follow("luigi", "mario");
-  nw.follow("luigi", "yoshi");
-  nw.follow("yoshi", "mario");
-  nw.follow("yoshi", "luigi");
+  CHECK(nw.follow("mario", "luigi"));
+  CHECK(nw.follow("mario", "yoshi"));
+  CHECK(nw.follow("luigi", "mario"));
+  CHECK(nw.follow("luigi", "yoshi"));
+  CHECK(nw.follow("yoshi", "mario"));
+  CHECK(nw.follow("yoshi", "luigi"));
+  CHECK(!nw.follow("DUMBPANDABEAR", "mario"));
+  CHECK(!nw.follow("mario", "wario"));
 
   // add a user who does not follow others
   nw.addUser("wario", "Wario");
+  CHECK(nw.follow("wario", "mario"));
 
   // add clone users who follow @mario
   for(int i = 2; i < 6; i++) {
       std::string usrn = "mario" + std::to_string(i);
       std::string dspn = "Mario " + std::to_string(i);
       nw.addUser(usrn, dspn);
-      nw.follow(usrn, "mario");
+      CHECK(nw.follow(usrn, "mario"));
   }
-  // additionally, make @mario2 follow @luigi
-  nw.follow("mario2", "luigi");
 }
